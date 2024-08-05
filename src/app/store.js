c6d45b1b7b54f99b286634d05f3963c18aae6455
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2024-08-06 00:07:34 +0300

    Yapılacaklar için Slice oluşturma

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,10 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice'
-
+import todoReducer from '../features/todo/todoSlice'
 export const store = configureStore({
   reducer: {
-   auth:authReducer
+   auth:authReducer,
+   todos:todoReducer
   },
   middleware:(getDefaultMiddleware)=>
     getDefaultMiddleware({
