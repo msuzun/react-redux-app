f5367305ce305985f1af76ae39a41b85b7521869
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2024-08-05 18:23:59 +0300

    firebase paketi kurulumu ve firebase config dosyası oluşturma

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import {initializeApp} from 'firebase/app'
+import {getAuth} from 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBC3gpdKI9fG4zitlcQySJtXWvVv8v-xVY",
+    authDomain: "react-redux-toolkit-57974.firebaseapp.com",
+    projectId: "react-redux-toolkit-57974",
+    storageBucket: "react-redux-toolkit-57974.appspot.com",
+    messagingSenderId: "1026252384849",
+    appId: "1:1026252384849:web:a6502460f090d020e42670"
+  };
+
+  const app = initializeApp(firebaseConfig)
+
+  const auth = getAuth(app)
+
+  export {auth}
