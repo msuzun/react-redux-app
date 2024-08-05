57c86447f3534b508ce68ba34e6c4376c2d3494a
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2024-08-06 00:44:59 +0300

    Yapılacakları Göstermek İçin Slice Oluşturma

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -26,6 +26,22 @@ export const todoSlice = createSlice({
             .addCase(createTodo.rejected,(state,action)=>{
                 
             })
+            .addCase(getTodos.pending,(state)=>{
+                state.isLoading = true
+            })
+            .addCase(getTodos.fulfilled,(state,action)=>{
+                state.isLoading = false
+                state.isSuccess = true
+                state.todos = action.payload
+                state.isError = false
+            })
+            .addCase(getTodos.rejected,(state,action)=>{
+                state.isLoading = false
+                state.isSuccess = false
+                state.message = action.payload
+                state.isError = true
+                state.todos = []
+            })
     }
 })
 
@@ -35,6 +51,17 @@ export const createTodo = createAsyncThunk('todos/create',async(todoData,thunkAP
         return await todoService.createTodo(todoData,user)
     } catch (error) {
         const message = error.message
+        return thunkAPI.rejectWithValue(message)
+    }
+})
+
+export const getTodos = createAsyncThunk('todos/getAll',async(_,thunkAPI)=>{
+    try {
+        const user = thunkAPI.getState().auth.user
+        return await todoService.getTodos(user)
+    } catch (error) {
+        const message = error.message
+
         return thunkAPI.rejectWithValue(message)
     }
 })
