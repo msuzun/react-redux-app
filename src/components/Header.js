6d23358a4f7a738b45f0eed32f8f6ac3958d0dac
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2024-08-05 19:22:05 +0300

    Çıkış İşlemini gerçekleştirme

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,49 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { FaSignInAlt, FaUser } from 'react-icons/fa'
+import { Link, useNavigate } from 'react-router-dom'
+import { FaSignInAlt, FaUser, FaSignOutAlt } from 'react-icons/fa'
+import { useSelector, useDispatch } from 'react-redux'
+import { logout, reset } from '../features/auth/authSlice'
 export default function Header() {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const { user } = useSelector((state) => state.auth)
+    const onLogout = () => {
+        dispatch(logout())
+        dispatch(reset())
+        navigate('/login')
+
+    }
     return (
         <header className='header'>
             <div className='Logo'>
                 <Link to="/">Aos Yapılacaklar</Link>
             </div>
             <ul>
-                <li>
-                    <Link to="/Login"><FaSignInAlt/>Giriş</Link>
-                </li>
-                <li>
-                    <Link to="/register"><FaSignInAlt/>Üye Ol</Link>
-                </li>
+                {user ? (
+                    <>
+                        <ul>
+                            <li>
+                                <span>Merhaba {user.displayName}</span>
+                            </li>
+                            <li>
+                                <button onClick={onLogout} className='btn'><FaSignOutAlt />Çıkış</button>
+                            </li>
+                        </ul>
+                    </>
+                ) : (
+                    <>
+                        <ul>
+                            <li>
+                                <Link to="/Login"><FaSignInAlt />Giriş</Link>
+                            </li>
+                            <li>
+                                <Link to="/register"><FaSignInAlt />Üye Ol</Link>
+                            </li>
+                        </ul>
+
+                    </>
+                )}
             </ul>
         </header>
 
